67db78242937548b20d3ee46a6ae2d5cf7ad8158
Javier Godoy <j.godoy277@gmail.com>
2023-10-15 23:09:56 +0200

    cambio nombre

---
+++ Configs/CrowdfundingTime_BaseBalanceFixConfig.py
@@ -0,0 +1,35 @@
+fileName = "CrowdfundingT_BaseBalanceFix.sol"
+contractName = "CrowdfundingBase"
+functions = [
+"Donate();",
+"GetFunds();",
+"Claim();",
+"dummy_balanceGTZero();",
+"dummy_balanceIsZero();",
+"t();",
+]
+statePreconditions = [
+"(max_block >= blockNumber)",
+"(max_block < blockNumber && goal <= balance)",
+"(max_block < blockNumber && !funded && goal > balance && countBackers > 0)",
+"balance > 0",
+"balance == 0",
+"true",
+]
+functionPreconditions = [
+"backers[msg.sender] == 0",
+"msg.sender == owner",
+"backers[msg.sender] != 0",
+"true",
+"true",
+"true"
+]
+functionVariables = ""
+tool_output = "Found a counterexample"
+
+statesModeState = []
+statesNamesModeState = []
+statePreconditionsModeState = []
+
+# epaExtraConditions = "address(this).balance == 0"
+txBound = 10

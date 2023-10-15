8872bb78406fef2cda3399e14eb67e3bc5c409b6
Javier Godoy <j.godoy277@gmail.com>
2023-10-15 23:10:32 +0200

    diff entre alloy y solidityabstractor

--- Benchmark_info.py
+++ Benchmark_info.py
@@ -19,7 +19,7 @@ def runCommand(command):
     return result
 
 def run(mode, params, extra=""):
-    global table, command, configName, REPETICIONES, TXBOUND_END
+    global table, command, configName, REPETICIONES, TXBOUND_END, TIME_OUT
     commandEpa = command + " " + params + " txbound="+str(TXBOUND_END) + " time_out="+str(TIME_OUT)
     modeName = mode + ("-" + extra if extra != "" else "")
     print("Modo " + modeName)
@@ -46,112 +46,145 @@ def run(mode, params, extra=""):
     
     table.append([name+"_k="+str(TXBOUND_END), modeName, avgEpa, statesCount , initEpa, finiStates, functions]) 
 
-configs = [
-
+def config_B1():
+    configs = [
     ###Benchmark1-original
-    # ["AssetTransferConfig",["s"]],
-    # ["BasicProvenanceConfig",["s"]],
-    # ["DigitalLockerConfig",["s"]],
-    # ["DefectiveComponentCounterConfig",["s"]],
-    # ["FrequentFlyerRewardsCalculatorConfig",["s"]],
-    # ["HelloBlockchainConfig",["s"]],
-    # ["RefrigeratedTransportationConfig",["s"]],
-    # ["RoomThermostatConfig",["s"]],
-    # ["SimpleMarketplaceConfig",["s"]],
+    ["AssetTransferConfig",["s"]],
+    ["BasicProvenanceConfig",["s"]],
+    ["DefectiveComponentCounterConfig",["s"]],
+    ["DigitalLockerConfig",["s"]],    
+    ["FrequentFlyerRewardsCalculatorConfig",["s"]],
+    ["HelloBlockchainConfig",["s"]],
+    ["RefrigeratedTransportationConfig",["s"]],
+    ["RoomThermostatConfig",["s"]],
+    ["SimpleMarketplaceConfig",["s"]],
     ###Benchmark1-fixed
-    # ["AssetTransferFixedConfig",["s"]],
-    # ["BasicProvenanceFixedConfig",["s"]],
-    # ["DigitalLockerFixedConfig",["s"]],
-    # ["DefectiveComponentCounterFixedConfig",["s"]],
-    # ["HelloBlockchainFixedConfig",["s"]],
-    # ["RefrigeratedTransportationFixedConfig",["s"]],
-    # ["SimpleMarketplaceFixedConfig",["s"]],
-    
+    ["AssetTransferFixedConfig",["s"]],
+    ["BasicProvenanceFixedConfig",["s"]],
+    ["DefectiveComponentCounterFixedConfig",["s"]],
+    ["DigitalLockerFixedConfig",["s"]],
+    ["HelloBlockchainFixedConfig",["s"]],
+    ["RefrigeratedTransportationFixedConfig",["s"]],
+    ["SimpleMarketplaceFixedConfig",["s"]],
+    ]
+    return configs
 
+def config_B2():
+    configs = [
+        # Benchmark2-original
+        ["RefundEscrowConfig", ["s"]],
+        ["RefundEscrowConfig", ["e"]],
+        ["RefundEscrowWithdrawConfig", ["e"]],
+        ["EscrowVaultConfig", ["s"]],
+        ["EscrowVaultConfig", ["e"]],
+        ["EPXCrowdsaleConfig", ["s"]],
+        ["EPXCrowdsaleConfig", ["e"]],
+        ["EPXCrowdsaleIsCrowdsaleClosedConfig", ["e"]],
+        ["CrowdfundingTime_BaseConfig", ["e"]],
+        ["CrowdfundingTime_BaseBalanceConfig", ["e"]],
+        ["CrowdfundingTime_BaseBalanceFixConfig", ["e"]],
+        ["ValidatorAuctionConfig", ["s"]],
+        ["ValidatorAuctionConfig", ["e"]],
+        ["ValidatorAuction_withdrawConfig", ["e"]],
+        ["SimpleAuctionConfig", ["e"]],
+        ["SimpleAuctionTimeConfig", ["e"]],
+        ["SimpleAuctionEndedConfig", ["e"]],
+        ["SimpleAuctionHBConfig", ["s"]],
+        ["AuctionConfig", ["e"]],
+        ["AuctionEndedConfig", ["e"]],
+        ["AuctionWithdrawConfig", ["e"]],
+        ["RockPaperScissorsConfig", ["s"]],
+        ["RockPaperScissorsConfig", ["e"]],
+        
+        # ["Crowdfunding_BaseConfig", ["e"]],    
+        
+        # # Benchmark2-PA
+        # ["Crowdfunding_BaseConfig_models", ["s", "e"]],    
+        # ["CrowdfundingTime_BaseBalanceConfigFix_states", ["s"]],
+        # ["CrowdfundingTimeClaimBakersRefinementConfig", ["e"]],
+        # ["CrowdfundingTimeClaimRefinementConfig", ["e"]],
+        # ["CrowdfundingTimeConfig", ["e"]],
+        # ["CrowdfundingTimeDonateRefinementConfig", ["e"]],
+        # ["CrowdfundingTimeReentrancyConfig", ["e"]],
+        # ["CrowdfundingTimeReentrancyFixedConfig", ["e"]],
+        # ["CrowdfundingTimeReentrancyFixedMutexConfig", ["e"]],
+    ]
+    return configs
 
-    # Benchmark2-original
-    # ["RefundEscrowConfig", ["e"]],
-    # ["EscrowVaultConfig", ["e"]],
-    # ["EPXCrowdsaleConfig", ["e"]],
-    # ["Crowdfunding_BaseConfig", ["e"]],
-    # ["ValidatorAuctionConfig", ["e"]],
-    # ["SimpleAuctionConfig", ["e"]],
-    # ["AuctionConfig", ["e"]],
-    # ["RockPaperScissorsConfig", ["e"]],
-    
-    # # Benchmark2-PA
-    # ["AuctionWithdrawConfig", ["e"]],
-    # ["AuctionEndedConfig", ["e"]],
-    # ["Crowdfunding_BaseConfig_models", ["s", "e"]],
-    # ["CrowdfundingTime_BaseBalanceConfig", ["e"]],
-    # ["CrowdfundingTime_BaseBalanceConfigFix", ["e"]],
-    # ["CrowdfundingTime_BaseBalanceConfigFix_states", ["s"]],
-    # ["CrowdfundingTime_BaseConfig", ["e"]],
-    # ["CrowdfundingTimeClaimBakersRefinementConfig", ["e"]],
-    # ["CrowdfundingTimeClaimRefinementConfig", ["e"]],
-    # ["CrowdfundingTimeConfig", ["e"]],
-    # ["CrowdfundingTimeDonateRefinementConfig", ["e"]],
-    # # ["CrowdfundingTimeReentrancyConfig", ["e"]],
-    # # ["CrowdfundingTimeReentrancyFixedConfig", ["e"]],
-    # # ["CrowdfundingTimeReentrancyFixedMutexConfig", ["e"]],
-    # ["EPXCrowdsaleConfig", ["s"]],
-    # ["EPXCrowdsaleIsCrowdsaleClosedConfig", ["e"]],
-    # ["EscrowVaultConfig", ["s"]],
-    # ["RefundEscrowConfig", ["s"]],
-    # ["RefundEscrowWithdrawConfig", ["e"]],
-    # ["RockPaperScissorsConfig", ["e", "s"]],
-    # ["SimpleAuctionTimeConfig", ["e"]],
-    # ["SimpleAuctionHBConfig", ["s"]],
-    # ["ValidatorAuctionConfig_withdraw", ["e"]],
-    
-]
+def rename_configs(config):
+    configs = []
+    for c in config:
+        mode = "e" if c.endswith("epa") else "s"
+        configs.append([c.replace("_Mode.epa", "Config").replace("_Mode.states", "Config"), mode])
+    return configs
 
 table = [['Config', 'Mode' ,'Time', 'Inital pre count' , 'Pre count after true', 'Reduce Pr count', 'Functions count']]
-
+cofigs = []
+command = ""
+configName = ""
 REPETICIONES = 1
 TXBOUND_INIT = 4
-TXBOUND_END = 10 #Include
-TIME_OUT = 600
-init = time.time()
+TXBOUND_END = 4 #Include
+TIME_OUT = 300
 
-for config in configs:
-    configName = config[0]
-    modes = config[1]
-    print("Corriendo " + configName)
-    command = "python.exe .\Tesis.py " + configName + " -t" #windows
-    # command = "python3 Tesis.py " + configName + " -t"
+def main(subjects_config, repeticiones=1, txbound_init=4, txbound_end=4, timeout=300):
+    global table, command, configName, REPETICIONES, TXBOUND_END, TIME_OUT
+    configs = rename_configs(subjects_config)
+    REPETICIONES = repeticiones
+    TXBOUND_INIT = txbound_init
+    TXBOUND_END = txbound_end
+    TIME_OUT = timeout
+    init = time.time()
 
-    upper_bound = TXBOUND_END
-    for curr_txBound in range(TXBOUND_INIT, upper_bound+1):
-        TXBOUND_END = curr_txBound
-        for mode in modes:
-            if mode == "e":
-                run("epa", "-e")
-                # run("epa", "-e -rt", "noReduceTrue")
-                # run("epa", "-e -re", "noReduceEqual")
-                # run("epa", "-e -rte", "noReduceTrueEqual")
-                # run("epa", "-e -rs", "noReduceStates")
-                # run("epa", "-e -ra", "noReduceAll")
-            if mode == "s":
-                run("states", "-s")
-        
-            # now = str(datetime.datetime.now()).replace(":","-")
-            # with open('Tiempos-'+ now +'.txt', 'w') as outputfile:
-            #     print(tabulate(table, headers='firstrow', tablefmt='simple'), file=outputfile)
-            # if mode == "e":
-            #     run("epa", "-e -rs", "noReduceStates")
-        
-            # now = str(datetime.datetime.now()).replace(":","-")
-            # with open('Tiempos-'+ now +'.txt', 'w') as outputfile:
-            #     print(tabulate(table, headers='firstrow', tablefmt='simple'), file=outputfile)
+    all_names_ok = True
+    for config in configs:
+        configName = config[0]
+        if not os.path.exists(os.path.join("Configs", configName+".py")):
+            print("No existe el archivo: "+ configName)
+            all_names_ok = False
 
-            # if mode == "e":
-            #     run("epa", "-e -ra", "noReduceAll")
-    
-        
-now = str(datetime.datetime.now()).replace(":","-")
-with open('Tiempos-'+ now +'.txt', 'w') as outputfile:
-    print(tabulate(table, headers='firstrow', tablefmt='simple'), file=outputfile)
+    if not all_names_ok:
+        exit(1)
+
+    for config in configs:
+        configName = config[0]
+        modes = config[1]
+        print("Corriendo " + configName)
+        command = "python .\SolidityAbstractor.py " + configName + " -b"
+
+        upper_bound = TXBOUND_END
+        for curr_txBound in range(TXBOUND_INIT, upper_bound+1):
+            TXBOUND_END = curr_txBound
+            for mode in modes:
+                if mode == "e":
+                    run("epa", "-e")
+                    # run("epa", "-e -rt", "noReduceTrue")
+                    # run("epa", "-e -re", "noReduceEqual")
+                    # run("epa", "-e -rte", "noReduceTrueEqual")
+                    # run("epa", "-e -rs", "noReduceStates")
+                    # run("epa", "-e -ra", "noReduceAll")
+                if mode == "s":
+                    run("states", "-s")
             
-end = time.time()
-print("Tiempo total: " + str(end - init) + "segs")
+                # now = str(datetime.datetime.now()).replace(":","-")
+                # with open('Tiempos-'+ now +'.txt', 'w') as outputfile:
+                #     print(tabulate(table, headers='firstrow', tablefmt='simple'), file=outputfile)
+                # if mode == "e":
+                #     run("epa", "-e -rs", "noReduceStates")
+            
+                # now = str(datetime.datetime.now()).replace(":","-")
+                # with open('Tiempos-'+ now +'.txt', 'w') as outputfile:
+                #     print(tabulate(table, headers='firstrow', tablefmt='simple'), file=outputfile)
+
+                # if mode == "e":
+                #     run("epa", "-e -ra", "noReduceAll")
+        
+            
+    now = str(datetime.datetime.now()).replace(":","-")
+    with open('Tiempos-'+ now +'.txt', 'w') as outputfile:
+        print(tabulate(table, headers='firstrow', tablefmt='simple'), file=outputfile)
+                
+    end = time.time()
+    print("Tiempo total: " + str(end - init) + "segs")
+
+# main(config_B2(), REPETICIONES, 4, 4, 300)
--- Differ.py
+++ Differ.py
@@ -1,6 +1,7 @@
 import os
 import networkx as nx
 import pydot
+import Benchmark_info
 
 def load_dot_file(file_path):
     graph = pydot.graph_from_dot_file(file_path)[0]
@@ -23,13 +24,170 @@ def are_dot_files_equivalent(file1, file2):
     
     return nx.is_isomorphic(g1, g2)
 
+def encontrar_nodos_no_equivalentes(grafo1, grafo2):
+    nodos_no_equivalentes = []
+    
+    for nodo1 in grafo1.nodes:
+        equivalente = False
+        for nodo2 in grafo2.nodes:
+            if nx.is_isomorphic(grafo1.subgraph([nodo1]), grafo2.subgraph([nodo2])):
+                equivalente = True
+                break
+        if not equivalente:
+            nodos_no_equivalentes.append(nodo1)
+    
+    return nodos_no_equivalentes
+
+
+def differences(dot1, dot2):
+    G1 = load_dot_file(dot1)
+    G2 = load_dot_file(dot2)
+
+    # nodos1 = set(G1.nodes)
+    # nodos2 = set(G2.nodes)
+
+        # Obtiene los nodos de cada grafo
+    nodes1 = set(node for node in G1.nodes())
+    nodes2 = set(node for node in G2.nodes())
+
+    # Encuentra los nodos diferentes entre los dos grafos
+    # diferencias1 = nodes1 - nodes2
+    # diferencias2 = nodes2 - nodes1
+
+    # print("Nodos diferentes en el primer grafo:", diferencias1)
+    # print("Nodos diferentes en el segundo grafo:", diferencias2)
+    nodos_diferentes = set(G1.nodes) ^ set(G2.nodes)
+
+
+
+    # nodos_no_equivalentes = encontrar_nodos_no_equivalentes(G1, G2)
+    # diferencias = nx.difference(G1, G2)
+    # nodos_diferentes = nodos1.symmetric_difference(nodos2)
+    # print("Nodos diferentes entre los dos grafos:")
+    for nodo in nodos_diferentes:
+        print(nodo)
+
+
+    # Obtener los nodos que son diferentes
+    # nodos_diferentes = list(diferencias.nodes)
+
+
+
+    # Crear un mapeo de nodos entre los dos grafos
+    # mapeo_nodos = {}
+    # for nodo1 in G1.nodes:
+    #     for nodo2 in G2.nodes:
+    #         if G1.nodes[nodo1] == G2.nodes[nodo2]:
+    #             mapeo_nodos[nodo1] = nodo2
+                
+    # diferencias = nx.difference(G1, G2)
+    # nodos_diferentes = [mapeo_nodos[nodo] for nodo in diferencias.nodes]
+
+    # print()
+    # print(nodos_no_equivalentes)
+    # if nodos_no_equivalentes:
+    #     print("Nodos no equivalentes:")
+    #     for nodo in nodos_no_equivalentes:
+    #         print(nodo)
+    # print()
+    # print()
+
 
 def diff(file1Name, file2Name):
     if not are_dot_files_equivalent(file1Name, file2Name):
         print(file1Name + " is different from " + file2Name)
+        return True
+        # differences(file1Name, file2Name)
+    return False
+# entries = os.listdir('graph/k_10/to_600/')
+# for i in range(9,10):
+#     for entry in entries:
+#         if not entry.endswith("pdf") and not entry.endswith("txt"):
+#             diff("graph/k_10/to_600/" + entry, "graph/k_{}/to_600/".format(i) + entry)
 
-entries = os.listdir('graph/k_10/to_600/')
-for i in range(9,10):
-    for entry in entries:
-        if not entry.endswith("pdf") and not entry.endswith("txt"):
-            diff("graph/k_10/to_600/" + entry, "graph/k_{}/to_600/".format(i) + entry)
+def get_alloy_subjects(subjects, benchmark):
+    solidityabstractor_subjects_b1 = ["AssetTransferFixed", "AssetTransfer", "BasicProvenanceFixed", "BasicProvenance", "DefectiveComponentCounterFixed", "DefectiveComponentCounter", "DigitalLockerFixed", "DigitalLocker", "FrequentFlyerRewardsCalculator", "HelloBlockchainFixed", "HelloBlockchain", "RefrigeratedTransportationFixed", "RefrigeratedTransportation", "RoomThermostat", "SimpleMarketplaceFixed", "SimpleMarketplace"]
+    alloy_subjects_b1 = ["AssetTransfer_fixed", "AssetTransfer_original", "BasicProvenance_fixed", "BasicProvenance_original", "DefectiveComponentCounter_fixed", "DefectiveComponentCounter_original", "DigitalLocker_fixed", "DigitalLocker_original", "FrequentFlyerRewardsCalculator_original", "HelloBlockchain_fixed", "HelloBlockchain_original", "RefrigeratedTransportation_fixed", "RefrigeratedTransportation_original", "RoomThermostat_original", "SimpleMarketplace_fixed", "SimpleMarketplace_original"]
+    
+    solidityabstractor_subjects_b2 = ["RefundEscrow_Mode.states", "RefundEscrow_Mode.epa", "RefundEscrowWithdraw_Mode.epa", "EscrowVault_Mode.states", "EscrowVault_Mode.epa", "EPXCrowdsale_Mode.states", "EPXCrowdsale_Mode.epa", "EPXCrowdsaleIsCrowdsaleClosed_Mode.epa", "CrowdfundingTime_Base_Mode.epa", "CrowdfundingTime_BaseBalance_Mode.epa", "CrowdfundingTime_BaseBalanceFix_Mode.epa", "ValidatorAuction_Mode.states", "ValidatorAuction_Mode.epa", "ValidatorAuction_withdraw_Mode.epa", "SimpleAuction_Mode.epa", "SimpleAuctionTime_Mode.epa", "SimpleAuctionEnded_Mode.epa", "SimpleAuctionHB_Mode.states", "Auction_Mode.epa", "AuctionEnded_Mode.epa", "AuctionWithdraw_Mode.epa", "RockPaperScissors_Mode.states", "RockPaperScissors_Mode.epa"]
+    alloy_subjects_b2 = ["RefundEscrow", "RefundEscrow_EPA", "RefundEscrow_withdrawA", "EscrowVault", "EscrowVault_EPA", "EPXCrowdsale", "EPXCrowdsale_EPA", "EPXCrowdsale_EPA_isCrowdSaleClosed", "CrowdfundingTime_Base", "CrowdfundingTime_BaseBalance", "CrowdfundingTime_BaseBalanceFix", "ValidatorAuction_EPA", "ValidatorAuction", "ValidatorAuction_WithdrawA+NotA", "SimpleAuction_EPA", "SimpleAuction_EPA_time", "SimpleAuction+Ended", "SimpleAuction+Ended+highestBidder", "Auction_EPA", "Auction_EPA+predicateEnded", "Auction_EPA_withdrawA", "RockPaperScissors+predicateOneWinner", "RockPaperScissors_EPA"]
+    
+    to_find_verisol = []
+    to_find_alloy = []
+    if benchmark == "B1":
+        to_find_verisol = solidityabstractor_subjects_b1
+        to_find_alloy = alloy_subjects_b1
+    elif benchmark == "B2":
+        to_find_verisol = solidityabstractor_subjects_b2
+        to_find_alloy = alloy_subjects_b2
+    else:
+        print("solo benchmark B1 o B2!")
+        exit(1)
+    ret = []
+    for i in range(len(subjects)):
+        for j in range(len(to_find_verisol)):
+            if subjects[i] == to_find_verisol[j]:
+                ret.append(to_find_alloy[j])
+    return ret
+    
+def get_diff(solidityabstractor_subjects, benchmark):
+    path_solidityabstractor = "C:\\Users\\JavierGodoy\\Repos\\SolidityAbstractor\\graph\\k_4\\to_300"
+    path_alloy = "C:\\Users\\JavierGodoy\\Repos\\sosym23\\models_alloy\\B2\\"
+    cant_dif = 0
+    subjects_diff = []
+
+    alloy_subjects = get_alloy_subjects(solidityabstractor_subjects, benchmark)
+    if len(alloy_subjects) != len(solidityabstractor_subjects):
+        print("Diferente cantidad de subjects alloy y solidityabstractor")
+        exit(1)
+    for i in range(len(alloy_subjects)):
+        if solidityabstractor_subjects[i].endswith("_Mode.states") or solidityabstractor_subjects[i].endswith("_Mode.epa"):
+            solidityabstractor_dot = os.path.join(path_solidityabstractor, solidityabstractor_subjects[i])
+        else:
+            print(r"agregar modo states o epa al subject '{solidityabstractor_subjects[i]}'")
+            # file_dot = os.path.join(path_solidityabstractor,solidityabstractor_subjects[i]+"_Mode.states")
+            # solidityabstractor_dot = file_dot if os.path.exists(file_dot) else file_dot.replace("_Mode.states","_Mode.epa")
+            
+        alloy_dot = os.path.join(path_alloy,alloy_subjects[i],alloy_subjects[i]+"_original.dot")
+        if not os.path.exists(alloy_dot) or not os.path.exists(solidityabstractor_dot):
+            if not os.path.exists(alloy_dot):
+                print(f"NO EXISTE ARCHIVO ALLOY {alloy_dot}")
+            if not os.path.exists(solidityabstractor_dot):
+                print(f"NO EXISTE ARCHIVO VERISOL {solidityabstractor_dot}")
+        else:
+            ret = diff(solidityabstractor_dot, alloy_dot)
+            if ret:
+                subjects_diff.append(solidityabstractor_subjects[i])
+                cant_dif += 1
+    print("total diff: "+ str(cant_dif))
+    print("diffs: ")
+    print(subjects_diff)
+    return subjects_diff
+
+# subjects = ["RefundEscrow_Mode.states", "RefundEscrow_Mode.epa", "RefundEscrowWithdraw_Mode.epa", "EscrowVault_Mode.states", "EscrowVault_Mode.epa", "EPXCrowdsale_Mode.states", "EPXCrowdsale_Mode.epa", "EPXCrowdsaleIsCrowdsaleClosed_Mode.epa", "CrowdfundingTime_Base_Mode.epa", "CrowdfundingTime_BaseBalance_Mode.epa", "CrowdfundingTime_BaseBalanceFix_Mode.epa", "ValidatorAuction_Mode.states", "ValidatorAuction_Mode.epa", "ValidatorAuction_withdraw_Mode.epa", "SimpleAuction_Mode.epa", "SimpleAuctionTime_Mode.epa", "SimpleAuctionEnded_Mode.epa", "SimpleAuctionHB_Mode.states", "Auction_Mode.epa", "AuctionEnded_Mode.epa", "AuctionWithdraw_Mode.epa", "RockPaperScissors_Mode.states", "RockPaperScissors_Mode.epa"]
+subjects = ['RefundEscrow_Mode.epa', 'RefundEscrowWithdraw_Mode.epa', 'EPXCrowdsale_Mode.states', 'EPXCrowdsale_Mode.epa', 'EPXCrowdsaleIsCrowdsaleClosed_Mode.epa', 'CrowdfundingTime_Base_Mode.epa', 'CrowdfundingTime_BaseBalance_Mode.epa', 'CrowdfundingTime_BaseBalanceFix_Mode.epa', 'ValidatorAuction_Mode.states', 'ValidatorAuction_Mode.epa', 'ValidatorAuction_withdraw_Mode.epa', 'SimpleAuction_Mode.epa', 'SimpleAuctionTime_Mode.epa', 'SimpleAuctionEnded_Mode.epa', 'Auction_Mode.epa', 'AuctionEnded_Mode.epa', 'AuctionWithdraw_Mode.epa']
+Benchmark = "B2"
+total_diff_antes = len(subjects)
+total_diff_despues = len(subjects)
+k_parameter = 4
+# first=True
+first = False
+segunda_vez_igual = False
+while (total_diff_despues <= total_diff_antes and not segunda_vez_igual) or first:
+    if not first:
+        k_parameter *= 2
+        if total_diff_antes == total_diff_despues:
+            segunda_vez_igual = True
+    first = False
+    total_diff_antes = total_diff_despues
+    total_subjects = len(subjects)
+    Benchmark_info.main(subjects, 1, k_parameter, k_parameter, 300)
+    subjects = get_diff(subjects, Benchmark)
+    total_diff_despues = len(subjects)
+    tempFileName = "diff_result_"+Benchmark+".txt"
+    with open(os.path.join(tempFileName), 'a+') as file:
+        file.write(f"Con k={k_parameter}\n")
+        file.write(f"total diff: {total_diff_despues} de un total de {total_subjects}\n")
+        file.write("subjects diferentes:\n")
+        file.write(str(subjects)+"\n")
+        file.close()

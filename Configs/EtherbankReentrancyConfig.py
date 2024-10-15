ba315979725b3158e583b9bb2b9b1dc29b9ec30d
Javi Godoy <j.godoy277@gmail.com>
2024-10-15 20:18:11 -0300

    agrego ejemplo reentrada etherbank

---
+++ Configs/EtherbankReentrancyConfig.py
@@ -0,0 +1,33 @@
+fileName = "EtherbankReentrancy.sol"
+contractName = "EtherBank"
+functions = [
+"addToBalance();",
+"withdrawBalance_Init();",
+"withdrawBalance_End();",
+"dummy_balanceGTZero();",
+"dummy_balanceIsZero();"
+]
+statePreconditions = [
+"true",
+"balance > 0",
+"senders_reentrant.length > 0",
+"balance > 0",
+"balance == 0"
+]
+functionPreconditions = [
+"true",
+"true",
+"senders_reentrant[senders_reentrant.length-1] == msg.sender",
+"true",
+"true"
+]
+functionVariables = ""
+# functionVariables = "uint n"
+tool_output = "Found a counterexample"
+
+statesModeState = []
+statesNamesModeState = []
+statePreconditionsModeState = []
+
+# epaExtraConditions = "address(this).balance == 0"
+txBound = 10
